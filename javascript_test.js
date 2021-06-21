712112d7f986d1f682715985011c1c6641b5a468
Oliver-Feighan <oliverfeighan1996@gmail.com>
2021-06-21 09:35:24 +0100

    make calculator

--- javascript_test.js
+++ javascript_test.js
@@ -1,18 +1,64 @@
-<!DOCTYPE html>
-<html>
-<body>
+var output = {val1:"", val2:"", operation:""};
 
-<h2>JavaScript in Body</h2>
+function evaluate(){
+	const rhs = parseFloat(output.val1)
+	const lhs = parseFloat(output.val2)
 
-<p id="demo">A Paragraph.</p>
+	switch(output.operation) {
+		case "+":
+			return rhs + lhs;
+		case "-":
+			return rhs - lhs;
+		case "*":
+			return rhs * lhs;
+		case "/":
+			return rhs / lhs;
+	}
 
-<button type="button" onclick="myFunction()">Try it</button>
-
-<script>
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
-</script>
 
-</body>
-</html> 
+function write_to_output(input) {
+
+	switch (input) {
+		case "C":
+			output.val1 = ""
+			output.val2 = ""
+			output.operation = ""
+
+			document.getElementById("output").innerHTML = "\n"
+
+			break;
+		case "+":
+			output.operation = input;
+			break;
+		case "-":
+			output.operation = input;
+			break;
+		case "*":
+			output.operation = input;
+			break;
+		case "/":
+			output.operation = input;
+			break;
+		case "=":
+			const result = evaluate();
+			output.val1 = result;
+			output.operation = "";
+			output.val2 = "";
+			break;
+		default:
+			if (output.operation !== ""){
+				output.val2 += input;
+				break;
+			}
+			else {
+				output.val1 += input;
+				break;
+			}
+		break;
+
+	}
+
+	document.getElementById("output").innerHTML = output.val1 + " " + output.operation + " " + output.val2
+
+}
